4572253e5b902153929fd807d57b410ff22f9dd7
Richard Spindler <richard.spindler@gmail.com>
2018-11-09 23:13:18 +0100

    example thrift service

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,42 @@
-int main(int argc, const char *argv[])
-{
-	
-	return 0;
+#include "MultiplicationService.h"
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+
+using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
+
+using namespace  ::tutorial;
+
+class MultiplicationServiceHandler : virtual public MultiplicationServiceIf {
+ public:
+  MultiplicationServiceHandler() {
+    // Your initialization goes here
+  }
+
+  int32_t multiply(const int32_t n1, const int32_t n2) {
+    // Your implementation goes here
+    printf("multiply\n");
+    return n1 * n2;
+  }
+
+};
+
+int main(int argc, char **argv) {
+  int port = 9090;
+  auto handler = std::make_shared<MultiplicationServiceHandler>(); //::apache::thrift::stdcxx::shared_ptr<MultiplicationServiceHandler> handler(new MultiplicationServiceHandler());
+  //::apache::thrift::stdcxx::shared_ptr<MultiplicationServiceHandler> handler(new MultiplicationServiceHandler());
+  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new MultiplicationServiceProcessor(handler));
+  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+
+  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+  server.serve();
+  return 0;
 }
+
+
---
+++ src/multiplication.thrift
@@ -0,0 +1,7 @@
+namespace cpp tutorial
+
+service MultiplicationService
+{
+	i32 multiply(1:i32 n1, 2:i32 n2),
+}
+
